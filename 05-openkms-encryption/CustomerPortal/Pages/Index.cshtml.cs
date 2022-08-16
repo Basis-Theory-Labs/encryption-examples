256e8356c487d79a7df619e4c6435dae467053eb
Brandon Weber <brandon@basistheory.com>
2022-08-16 10:50:01 -0500

    Add lib files

--- 05-openkms-encryption/CustomerPortal/Pages/Index.cshtml.cs
+++ 05-openkms-encryption/CustomerPortal/Pages/Index.cshtml.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace NoEncryption.Pages;
+namespace CustomerPortal.Pages;
 
 public class IndexModel : PageModel
 {
--- 05-openkms-encryption/CustomerPortal/Pages/Privacy.cshtml.cs
+++ 05-openkms-encryption/CustomerPortal/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace NoEncryption.Pages;
+namespace CustomerPortal.Pages;
 
 public class PrivacyModel : PageModel
 {
